a3b9f09ac4273213394ec7f7fa7e844f42d26043
Christine Perry <christine@nomikai.com>
2024-02-05 13:56:12 -1000

    Update nav

--- packages/blog-starter-kit/themes/enterprise/components/publication-logo.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/publication-logo.tsx
@@ -24,7 +24,7 @@ export const PublicationLogo = ({ isSidebar }: { isSidebar?: boolean }) => {
 				{PUBLICATION_LOGO ? (
 					<>
 						<img
-							className="block w-32 shrink-0"
+							className="block w-32 shrink-0 md:w-40"
 							alt={publication.title}
 							src={resizeImage(PUBLICATION_LOGO, { w: 320, h: 80 })}
 						/>
